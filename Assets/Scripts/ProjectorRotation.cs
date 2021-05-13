45ce1cbecf8f4759b662b774640a10fe9073ee33
Astrodave73 <Astrodave73@github.com>
2021-05-13 03:43:15 -0500

    Proyector listo

--- Assets/Scripts/ProjectorRotation.cs
+++ Assets/Scripts/ProjectorRotation.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class ProjectorRotation : MonoBehaviour
 {
 
-    public float rotSpeed =1f;
+    float rotSpeed=1f;
     [SerializeField]private AnimationCurve rotationSpeed;
+    private float _time;
+     public float dissolveDuration;
 
-    [SerializeField] [Range(0, 1)] private float slider;
+    //[SerializeField]
+    [Range(0, 1)] private float slider;
 
    
     
@@ -18,14 +21,17 @@ public class ProjectorRotation : MonoBehaviour
     void Start()
     {
         mat = GetComponent<Renderer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    
+        _time += Time.deltaTime;
         transform.Rotate(0, rotationSpeed.Evaluate(rotSpeed)  , 0);
-        mat.material.SetFloat("_Level",slider);
-}
+        
+            mat.material.SetFloat("_Level",   slider+_time / dissolveDuration);
+           
+         
+    }
 }
