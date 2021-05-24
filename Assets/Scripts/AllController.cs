a811fdc5b2a7c46b1d2dc616a810ac4e270f7e21
Dagane <dagane8@hotmail.com>
2021-05-23 19:39:43 -0500

    Controller

--- Assets/Scripts/AllController.cs
+++ Assets/Scripts/AllController.cs
@@ -6,22 +6,38 @@ public class AllController : MonoBehaviour
 {
     [SerializeField]
     float speed;
-    [SerializeField]
+
     ParticleSystem[] ps;
     ParticleSystem.MainModule mainP;
+    [SerializeField]
+    GameObject disolver;
+    [SerializeField]
+    ParticleSystem fireRing;
+    [SerializeField]
+    Gradient gradient;
+
+    ProjectorRotation projectorR;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        projectorR = GetComponentInChildren<ProjectorRotation>();
+        ps = GetComponentsInChildren<ParticleSystem>();
         foreach(ParticleSystem p in ps)
         {
             mainP = p.main;
+            mainP.simulationSpeed = speed;
         }
+        projectorR.dissolveDuration = projectorR.dissolveDuration / speed;
     }
 
     // Update is called once per frame
     void Update()
-    {       
-        mainP.simulationSpeed = speed;
+    {
+        if (fireRing.isStopped)
+        {
+            disolver.SetActive(true);
+        }
     }
 }
