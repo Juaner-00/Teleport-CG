893aebaede65789f62d262f3db9a9ac0478eb9a6
Astrodave73 <Astrodave73@github.com>
2021-05-24 13:47:44 -0500

    Bloom corregido

--- Assets/Scripts/BloomEffect.cs
+++ Assets/Scripts/BloomEffect.cs
@@ -2,92 +2,87 @@
 using System;
 
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
-public class BloomEffect : MonoBehaviour
-{
-    [Range(1, 16)] public int iterations = 1;
+public class BloomEffect : MonoBehaviour {
 
-    RenderTexture[] textures = new RenderTexture[16];
+	const int BoxDownPrefilterPass = 0;
+	const int BoxDownPass = 1;
+	const int BoxUpPass = 2;
+	const int ApplyBloomPass = 3;
+	const int DebugBloomPass = 4;
 
-    public Shader bloomShader;
+	public Shader bloomShader;
 
-    [NonSerialized] Material bloom;
+	[Range(0, 10)]
+	public float intensity = 1;
 
-    const int BoxDownPrefilterPass = 0;
-    const int BoxDownPass = 1;
-    const int BoxUpPass = 2;
-    const int ApplyBloomPass = 3;
-    const int DebugBloomPass = 4;
+	[Range(1, 16)]
+	public int iterations = 4;
 
-    public bool debug;
+	[Range(0, 10)]
+	public float threshold = 1;
 
-    [Range(0, 10)] public float threshold = 1;
-    
-    [Range(0, 1)]
-    public float softThreshold = 0.5f;
-    
-    [Range(0, 10)]
-    public float intensity = 1;
+	//[Range(0, 1)]
+	 float softThreshold = 0.5f;
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        if (bloom == null)
-        {
-            bloom = new Material(bloomShader);
-            bloom.hideFlags = HideFlags.HideAndDontSave;
-        }
+	public bool debug;
 
-        /*bloom.SetFloat("_Threshold", threshold);
-        bloom.SetFloat("_SoftThreshold", softThreshold);*/
-        float knee = threshold * softThreshold;
-        Vector4 filter;
-        filter.x = threshold;
-        filter.y = filter.x - knee;
-        filter.z = 2f * knee;
-        filter.w = 0.25f / (knee + 0.00001f);
-        bloom.SetVector("_Filter", filter);
-        bloom.SetFloat("_Intensity", Mathf.GammaToLinearSpace(intensity));
+	RenderTexture[] textures = new RenderTexture[16];
 
-        int width = source.width / 2;
-        int height = source.height / 2;
-        RenderTextureFormat format = source.format;
-        RenderTexture currentDestination = textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
+	[NonSerialized]
+	Material bloom;
 
-        Graphics.Blit(source, currentDestination, bloom, BoxDownPrefilterPass);
-        RenderTexture currentSource = currentDestination;
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+		if (bloom == null) {
+			bloom = new Material(bloomShader);
+			bloom.hideFlags = HideFlags.HideAndDontSave;
+		}
 
-        int i = 1;
-        for (; i < iterations; i++)
-        {
-            width /= 2;
-            height /= 2;
-            if (height < 2)
-            {
-                break;
-            }
+		float knee = threshold * softThreshold;
+		Vector4 filter;
+		filter.x = threshold;
+		filter.y = filter.x - knee;
+		filter.z = 2f * knee;
+		filter.w = 0.25f / (knee + 0.00001f);
+		bloom.SetVector("_Filter", filter);
+		bloom.SetFloat("_Intensity", Mathf.GammaToLinearSpace(intensity));
 
-            currentDestination = textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
-            Graphics.Blit(currentSource, currentDestination, bloom, BoxDownPass);
-            // RenderTexture.ReleaseTemporary(currentSource);
-            currentSource = currentDestination;
-        }
+		int width = source.width / 2;
+		int height = source.height / 2;
+		RenderTextureFormat format = source.format;
 
-        for (i -= 2; i >= 0; i--)
-        {
-            currentDestination = textures[i];
-            textures[i] = null;
-            if (debug) {
-                Graphics.Blit(currentSource, destination, bloom, DebugBloomPass);
-            }
-            else
-            {
-                bloom.SetTexture("_SourceTex", source);
-                Graphics.Blit(currentSource, destination, bloom, ApplyBloomPass);
-            }
+		RenderTexture currentDestination = textures[0] =
+			RenderTexture.GetTemporary(width, height, 0, format);
+		Graphics.Blit(source, currentDestination, bloom, BoxDownPrefilterPass);
+		RenderTexture currentSource = currentDestination;
 
-            RenderTexture.ReleaseTemporary(currentSource);
-            currentSource = currentDestination;
-        }
+		int i = 1;
+		for (; i < iterations; i++) {
+			width /= 2;
+			height /= 2;
+			if (height < 2) {
+				break;
+			}
+			currentDestination = textures[i] =
+				RenderTexture.GetTemporary(width, height, 0, format);
+			Graphics.Blit(currentSource, currentDestination, bloom, BoxDownPass);
+			currentSource = currentDestination;
+		}
 
-        Graphics.Blit(currentSource, destination, bloom, BoxUpPass);
-    }
+		for (i -= 2; i >= 0; i--) {
+			currentDestination = textures[i];
+			textures[i] = null;
+			Graphics.Blit(currentSource, currentDestination, bloom, BoxUpPass);
+			RenderTexture.ReleaseTemporary(currentSource);
+			currentSource = currentDestination;
+		}
+
+		if (debug) {
+			Graphics.Blit(currentSource, destination, bloom, DebugBloomPass);
+		}
+		else {
+			bloom.SetTexture("_SourceTex", source);
+			Graphics.Blit(currentSource, destination, bloom, ApplyBloomPass);
+		}
+		RenderTexture.ReleaseTemporary(currentSource);
+	}
 }
